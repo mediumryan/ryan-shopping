647dbfe183cfc1745d7e6992177c3df03714ee14
Ryan <straightwind0726@gmail.com>
2024-01-05 16:47:35 +0900

    [Init] Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/index.css';
+import faker from 'faker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 상품 데이터를 담을 배열 생성
+    const products = [];
+
+    // 50개의 가상 상품 데이터 생성
+    for (let i = 0; i < 50; i++) {
+        const categoryList = [
+            'BAG',
+            'LEATHER',
+            'OUTER',
+            'TOP',
+            'BOTTOM',
+            'ACC',
+        ];
+        const product = {
+            id: i + 1,
+            name: faker.commerce.productName(),
+            price: faker.random.number({ min: 10000, max: 100000 }),
+            category: faker.random.arrayElement(categoryList),
+        };
+        products.push(product);
+    }
+
+    console.log(JSON.stringify(products, null, 2)); // 생성된 JSON 데이터를 확인합니다.
+
+    return <div>hi</div>;
 }
 
 export default App;

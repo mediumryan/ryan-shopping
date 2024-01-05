26e8929b72f33c0734212668587f81d53729d994
Ryan <straightwind0726@gmail.com>
2024-01-05 22:13:34 +0900

    [Test] Viewport scrolling Test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,101 @@
-import './CSS/index.css';
-import faker from 'faker';
+import { useEffect, useRef, useState } from 'react';
+import './CSS/test.css';
+
+import Dots from './Components/Dots';
 
 function App() {
-    // 상품 데이터를 담을 배열 생성
-    const products = [];
+    const DIVIDER_HEIGHT = 5;
+    const outerDivRef = useRef();
+    const [currentPage, setCurrentPage] = useState(1);
+    useEffect(() => {
+        const wheelHandler = (e) => {
+            e.preventDefault();
+            const { deltaY } = e;
+            const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+            const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
 
-    // 50개의 가상 상품 데이터 생성
-    for (let i = 0; i < 50; i++) {
-        const categoryList = [
-            'BAG',
-            'LEATHER',
-            'OUTER',
-            'TOP',
-            'BOTTOM',
-            'ACC',
-        ];
-        const product = {
-            id: i + 1,
-            name: faker.commerce.productName(),
-            price: faker.random.number({ min: 10000, max: 100000 }),
-            category: faker.random.arrayElement(categoryList),
+            if (deltaY > 0) {
+                // 스크롤 내릴 때
+                if (scrollTop >= 0 && scrollTop < pageHeight) {
+                    //현재 1페이지
+                    console.log('현재 1페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight + DIVIDER_HEIGHT,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setCurrentPage(2);
+                } else if (
+                    scrollTop >= pageHeight &&
+                    scrollTop < pageHeight * 2
+                ) {
+                    //현재 2페이지
+                    console.log('현재 2페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setCurrentPage(3);
+                } else {
+                    // 현재 3페이지
+                    console.log('현재 3페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                }
+            } else {
+                // 스크롤 올릴 때
+                if (scrollTop >= 0 && scrollTop < pageHeight) {
+                    //현재 1페이지
+                    console.log('현재 1페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                } else if (
+                    scrollTop >= pageHeight &&
+                    scrollTop < pageHeight * 2
+                ) {
+                    //현재 2페이지
+                    console.log('현재 2페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setCurrentPage(1);
+                } else {
+                    // 현재 3페이지
+                    console.log('현재 3페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight + DIVIDER_HEIGHT,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setCurrentPage(2);
+                }
+            }
         };
-        products.push(product);
-    }
-
-    console.log(JSON.stringify(products, null, 2)); // 생성된 JSON 데이터를 확인합니다.
-
-    return <div>hi</div>;
+        const outerDivRefCurrent = outerDivRef.current;
+        outerDivRefCurrent.addEventListener('wheel', wheelHandler);
+        return () => {
+            outerDivRefCurrent.removeEventListener('wheel', wheelHandler);
+        };
+    }, []);
+    return (
+        <div ref={outerDivRef} className="outer">
+            <Dots currentPage={currentPage} />
+            <div className="inner bg-yellow">1</div>
+            <div className="divider"></div>
+            <div className="inner bg-blue">2</div>
+            <div className="divider"></div>
+            <div className="inner bg-pink">3</div>
+        </div>
+    );
 }
 
 export default App;

9ee06c04fad2dfee1471754950555492f9190051
Ryan <straightwind0726@gmail.com>
2024-01-29 12:19:47 +0900

    [Update] Setting media query on the BookmarkButtons section

--- src/Components/Bookmark/BookmarkButtons.jsx
+++ src/Components/Bookmark/BookmarkButtons.jsx
@@ -35,6 +35,9 @@ const BookmarkButtonsWrapper = styled.div`
             border-color: transparent;
         }
     }
+    @media only screen and (min-width: 320px) and (max-width: 768px) {
+        padding: 1rem;
+    }
 `;
 
 export default function BookmarkButtons() {

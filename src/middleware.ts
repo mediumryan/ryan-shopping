179714171e54a55fcaef6fa031f064b3fa2de989
ryan <straightwind0726@gmail.com>
2025-04-23 19:55:36 +0900

    [Update] Login TEST

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,6 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|find-pw|/).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|find-pw|).*)',
   ],
 };
